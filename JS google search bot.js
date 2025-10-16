ea3e867e2e9e46e6b14f1187b157e43468337b19
tortashell-create <tortashell@hotmail.com>
2025-10-16 21:01:23 +1100

    Update JS google search bot.js

--- JS google search bot.js
+++ JS google search bot.js
@@ -1,3 +1,4 @@
-document.querySelector('textarea[name="q"]').value = "amazon.com.au"
+document.querySelector('textarea[name="q"]').value = "https://www.amazon.com.au/"
 document.querySelector('textarea[name="q"]').form.submit();
+
 
